ce838dd32e894c3469dcaee6933e50db1784cfc5
PhoenixHarsha02 <harshastrikes007@gmail.com>
2020-03-04 18:00:51 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-//var person = String(prompt("Please enter your name"));
+var person = String(prompt("Please enter your name"));
 if(person != ""){ 
 var a= prompt("Enter Start Slice");
 {
 if(isNaN(a)===true){
   alert("Sorry,not a number");
-  return prompt("Enter Start Slice");
+  var d= prompt("Enter Start Slice");
   }
-else {
+else if(isNan(d)==false) {
   var b= prompt("Enter End Slice");
  if(isNan(b)===true){
   alert("Sorry,not a number");
-  return prompt("Enter End Slice");
+  var e= prompt("Enter End Slice");
   }
   else{
-  var c=(person.slice(a,b));
+  var c=(person.slice(d,e));
   console.log(alert(c));
  }}}}
   
