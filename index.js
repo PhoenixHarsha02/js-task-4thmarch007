c574b9154ba64fe3a364f1f9fe345d1e652cf70b
PhoenixHarsha02 <harshastrikes007@gmail.com>
2020-03-04 17:52:29 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 if(person != ""){ 
 var a= prompt("Enter Start Slice");
 {
-if(isNaN(a)==true){
+if(isNaN(a)===true){
   alert("Sorry,not a number");
   return prompt("Enter Start Slice");
   }
 else {
   var b= prompt("Enter End Slice");
- if(isNan(b)==true){
+ if(isNan(b)===true){
   alert("Sorry,not a number");
   return prompt("Enter End Slice");
   }
