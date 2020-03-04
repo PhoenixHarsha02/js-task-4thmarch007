e8e43749a27d46374574cb95c7aee37f2f4d8ab4
PhoenixHarsha02 <harshastrikes007@gmail.com>
2020-03-04 17:51:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-task-4thmarch007
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-task-4thmarch007)
---
+++ index.js
@@ -0,0 +1,19 @@
+//var person = String(prompt("Please enter your name"));
+if(person != ""){ 
+var a= prompt("Enter Start Slice");
+{
+if(isNaN(a)==true){
+  alert("Sorry,not a number");
+  return prompt("Enter Start Slice");
+  }
+else {
+  var b= prompt("Enter End Slice");
+ if(isNan(b)==true){
+  alert("Sorry,not a number");
+  return prompt("Enter End Slice");
+  }
+  else{
+  var c=(person.slice(a,b));
+  console.log(alert(c));
+ }}}}
+  
